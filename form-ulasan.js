939c678ad0105ecf1fce8a7154b0cf816767e043
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-01-08 19:08:25 +0700

    Update form-ulasan.js

--- form-ulasan.js
+++ form-ulasan.js
@@ -23,7 +23,6 @@ document.write(`
 <button style="font-size: 100%; max-width: 90%; padding: 12px; width: 464px;" type="submit">Kirim Ulasan</button>
 </form>
 </div>
-</div>
 
 <script>document.getElementById("rD").value=new Date().toLocaleDateString();let ratingValue=0;const stareviews=document.querySelectorAll(".stareviews");stareviews.forEach(star=>{star.addEventListener("click",function(){ratingValue=this.getAttribute("data-value");updateStareviews(ratingValue)}),star.addEventListener("mouseover",function(){const value=this.getAttribute("data-value");updateStareviews(value)}),star.addEventListener("mouseout",function(){updateStareviews(ratingValue)})});function updateStareviews(value){stareviews.forEach(star=>{star.getAttribute("data-value")<=value?star.classList.add("selected"):star.classList.remove("selected")})}function sendReview(){const name=document.getElementById("name").value,rD=document.getElementById("rD").value,reviewText=document.getElementById("reviewText").value,currentUrl=window.location.href;if(0===ratingValue)return alert("Silakan pilih rating bintang!"),!1;const message=`Ulasan Baru:\n\nNama: ${name}\nTanggal: ${rD}\nRating: ${ratingValue} Bintang\nUlasan: ${reviewText}\nURL Halaman: ${currentUrl}`,phone="6281327707780",whatsappUrl=`https://wa.me/${phone}?text=${encodeURIComponent(message)}`;return window.open(whatsappUrl,"_blank"),!1}</script>
 `);
