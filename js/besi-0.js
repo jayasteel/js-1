f652f530739ef8cc6d3ed235c3513872a3bdfc9e
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:53:20 +0700

    Create besi-0.js

---
+++ js/besi-0.js
@@ -0,0 +1,2 @@
+<p>Apakah Anda sedang mencari <b>besi beton</b> berkualitas untuk kebutuhan konstruksi Anda?<br />
+Jayasteel menyediakan berbagai ukuran besi beton dengan standar SNI, siap membantu proyek Anda berjalan lancar. Kami menawarkan produk besi beton yang kuat, tahan lama, dan dapat diandalkan untuk berbagai keperluan pembangunan.</p>
