5fe0ed8c461d86bd61c739793f874da82981a3e6
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:59:54 +0700

    Create artikel-7.js

---
+++ js/artikel-7.js
@@ -0,0 +1,2 @@
+<p>Di Jayasteel, kami tidak hanya menyediakan material konstruksi, tetapi juga solusi untuk membuat bangunan Anda lebih kuat dan efisien.<br />
+Kami menawarkan berbagai produk yang sesuai dengan kebutuhan arsitektur dan konstruksi Anda, memastikan proyek selesai dengan hasil yang optimal.</p>
