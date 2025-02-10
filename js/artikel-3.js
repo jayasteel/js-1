0c1e93ea8792929d942bc1ad1ef00156d6050bd4
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:58:12 +0700

    Create artikel-3.js

---
+++ js/artikel-3.js
@@ -0,0 +1,2 @@
+<p>Ingin memastikan proyek konstruksi Anda berjalan lancar dan tepat waktu?<br />
+Dengan memilih material konstruksi berkualitas dari Jayasteel, Anda memastikan proyek berjalan sesuai rencana, dengan hasil yang kuat dan tahan lama. Kami menyediakan berbagai pilihan besi beton dan wiremesh untuk berbagai aplikasi konstruksi.</p>
