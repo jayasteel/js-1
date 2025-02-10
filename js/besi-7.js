581ed15dfd5de081cce1b8ac6aa66d4e871abecf
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:47:18 +0700

    Create besi-7.js

---
+++ js/besi-7.js
@@ -0,0 +1,2 @@
+<p>Ingin mendapatkan <b>besi beton</b> dengan harga kompetitif? Jayasteel adalah pilihan yang tepat!<br />
+Kami menawarkan berbagai ukuran besi beton, mulai dari diameter kecil hingga besar, untuk memastikan bahwa kebutuhan material Anda terpenuhi dengan sempurna.</p>
