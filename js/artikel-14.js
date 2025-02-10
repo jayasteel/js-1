4cd5b92802856c8211415f6b4ad4074b3e2f902e
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:47:34 +0700

    Create artikel-14.js

---
+++ js/artikel-14.js
@@ -0,0 +1,2 @@
+<p>Jayasteel memahami bahwa konstruksi yang sukses dimulai dengan pemilihan material yang tepat.<br />
+Kami menawarkan produk besi beton dan wiremesh yang kuat dan tahan lama, cocok untuk mendukung desain arsitektur Anda yang membutuhkan kekuatan ekstra.</p>
