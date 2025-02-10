c153838b288033d6248e0e9b66e5f3a9bd0f965e
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:51:11 +0700

    Create besi-12.js

---
+++ js/besi-12.js
@@ -0,0 +1,2 @@
+<p>Butuh <b>besi beton</b> untuk pembangunan rumah atau gedung?<br />
+Jayasteel menyediakan berbagai ukuran besi beton dengan standar SNI, siap mendukung proyek Anda. Produk kami terkenal dengan kualitas dan ketahanan yang optimal, cocok untuk semua jenis konstruksi.</p>
