be4d76d3a7f65b35d9792a1e5c9f3097a7bb38fd
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 13:28:07 +0700

    Update wiremesh-14.js

--- js/wiremesh-14.js
+++ js/wiremesh-14.js
@@ -1,2 +1,2 @@
-<p>Di Jayasteel, Anda dapat menemukan <b>wiremesh</b> berkualitas tinggi dengan harga terjangkau.<br />
+<p>Di Jayasteel, Anda dapat menemukan <b>wiremesh</b> berkualitas dengan harga terjangkau.<br />
 Kami menawarkan berbagai pilihan wiremesh untuk berbagai jenis konstruksi, baik untuk kebutuhan proyek rumah tinggal maupun gedung bertingkat.</p>
