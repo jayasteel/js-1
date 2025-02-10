221d900f2879eda2128a8fdf8e0459c0a640b302
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:58:42 +0700

    Create artikel-4.js

---
+++ js/artikel-4.js
@@ -0,0 +1,2 @@
+<p>Jayasteel menawarkan solusi material konstruksi yang dapat diandalkan untuk setiap proyek arsitektur.<br />
+Dengan produk berkualitas, kami mendukung Anda dalam membangun bangunan yang kokoh, efisien, dan sesuai dengan standar arsitektur yang diinginkan.</p>
