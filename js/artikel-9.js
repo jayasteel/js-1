2688d36e7bc0ff49b50a3e6436a2dd38fc1bc833
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 15:00:44 +0700

    Create artikel-9.js

---
+++ js/artikel-9.js
@@ -0,0 +1,2 @@
+<p>Ketika membangun atau merenovasi, kualitas material konstruksi sangat mempengaruhi daya tahan dan estetika bangunan.<br />
+Di Jayasteel, kami menyediakan berbagai jenis material seperti besi beton, wiremesh, dan lainnya yang telah teruji kualitasnya untuk mendukung proyek arsitektur Anda.</p>
