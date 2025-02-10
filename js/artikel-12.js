575a088b61911b900aeb8a56dda38022a105aca3
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:46:44 +0700

    Create artikel-12.js

---
+++ js/artikel-12.js
@@ -0,0 +1,2 @@
+<p>Keberhasilan proyek konstruksi Anda bergantung pada kualitas bahan yang digunakan.<br />
+Jayasteel menyediakan berbagai pilihan material konstruksi yang dapat disesuaikan dengan kebutuhan desain arsitektur Anda, mulai dari besi beton hingga wiremesh berkualitas tinggi.</p>
