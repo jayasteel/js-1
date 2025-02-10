5770d87374825c4cd8da302b40ea78a3b5b10401
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:59:02 +0700

    Create artikel-5.js

---
+++ js/artikel-5.js
@@ -0,0 +1,2 @@
+<p>Dalam dunia konstruksi, ketepatan dan kualitas sangat penting.<br />
+Jayasteel menyediakan berbagai pilihan material seperti besi beton, wiremesh, dan lainnya yang siap membantu Anda membangun struktur yang kuat dan tahan lama, sesuai dengan standar arsitektur yang diinginkan.</p>
