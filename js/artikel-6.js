29b1a077fce7147f75f2ca5e376826837c02660e
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:59:30 +0700

    Create artikel-6.js

---
+++ js/artikel-6.js
@@ -0,0 +1,2 @@
+<p>Material yang tepat bisa membuat perbedaan besar dalam hasil akhir proyek konstruksi.<br />
+Jayasteel menawarkan produk berkualitas yang memenuhi standar industri, dari besi beton hingga wiremesh, yang mendukung desain arsitektur modern maupun tradisional.</p>
