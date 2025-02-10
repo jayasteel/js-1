4cc831b06d73df0b8a481f2e8bd35d7b5bf2d8b1
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:51:41 +0700

    Create besi-13.js

---
+++ js/besi-13.js
@@ -0,0 +1,2 @@
+<p>Jayasteel menawarkan <b>besi beton</b> terbaik untuk segala jenis pembangunan!<br />
+Dengan berbagai pilihan diameter, kami memastikan Anda mendapatkan produk yang sesuai dengan kebutuhan konstruksi Anda. Hubungi kami untuk informasi lebih lanjut dan penawaran terbaik.</p>
