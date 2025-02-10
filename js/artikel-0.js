05e79c1262262307810450cf81bc6a6cd4660af0
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:53:54 +0700

    Update artikel-0.js

--- js/artikel-0.js
+++ js/artikel-0.js
@@ -1,2 +1,2 @@
-<p>Apakah Anda sedang merencanakan proyek konstruksi atau renovasi?<br />
+<p>Bagi Anda sedang merencanakan proyek konstruksi atau renovasi?<br />
 Kami di Jayasteel memiliki berbagai material berkualitas yang bisa mendukung setiap tahapan proyek Anda, mulai dari konstruksi dasar hingga finishing. Dapatkan material terbaik hanya di Jayasteel untuk hasil yang maksimal!</p>
