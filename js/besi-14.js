47fd2229aed01fc9874682a83ae70d0a3b9af408
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:52:03 +0700

    Create besi-14.js

---
+++ js/besi-14.js
@@ -0,0 +1,2 @@
+<p>Apakah Anda ingin membeli <b>besi beton</b> yang kuat dan tahan lama?<br />
+Jayasteel menyediakan berbagai pilihan besi beton dengan ukuran sesuai kebutuhan Anda. Dapatkan harga terbaik dan produk berkualitas hanya di Jayasteel untuk memastikan keberhasilan proyek konstruksi Anda.</p>
