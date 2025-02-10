f1afc5aa4167e6dbb043b56e98c9c2a06df0957d
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:47:04 +0700

    Create artikel-13.js

---
+++ js/artikel-13.js
@@ -0,0 +1,2 @@
+<p>Untuk mencapai hasil konstruksi yang memuaskan, Anda memerlukan bahan bangunan yang tahan lama dan kuat.<br />
+Jayasteel menyediakan material berkualitas yang sesuai dengan standar konstruksi dan arsitektur, memastikan proyek Anda berjalan dengan lancar dan hasil yang optimal.</p>
