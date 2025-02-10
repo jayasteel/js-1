982258a982fffc4c970bba8f9afa7d05b9541e31
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:50:02 +0700

    Create artikel-19.js

---
+++ js/artikel-19.js
@@ -0,0 +1,2 @@
+<p>Arsitektur dan konstruksi tidak hanya tentang estetika, tetapi juga daya tahan.<br />
+Di Jayasteel, kami menyediakan material yang tepat untuk memastikan bangunan Anda kuat dan tahan lama, sambil tetap sesuai dengan desain arsitektur yang diinginkan.</p>
