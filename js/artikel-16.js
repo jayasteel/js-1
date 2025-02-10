9a4036226b67fd332a23878485716765889d23f8
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:48:16 +0700

    Create artikel-16.js

---
+++ js/artikel-16.js
@@ -0,0 +1,2 @@
+<p>Dengan material yang tepat, Anda bisa membangun proyek konstruksi yang lebih efisien dan tahan lama.<br />
+Jayasteel menawarkan berbagai produk berkualitas yang memenuhi standar SNI, dari besi beton hingga wiremesh, untuk mendukung desain arsitektur terbaik Anda.</p>
