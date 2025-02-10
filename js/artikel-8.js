de77cced77c3564ee5f9c5a6284961b9bafd33d2
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 15:00:19 +0700

    Create artikel-8.js

---
+++ js/artikel-8.js
@@ -0,0 +1,2 @@
+<p>Setiap bangunan membutuhkan fondasi yang kuat, dan itu dimulai dengan pemilihan material yang tepat.<br />
+Jayasteel menyediakan material konstruksi terbaik yang mendukung arsitektur dan desain bangunan, mulai dari tahap dasar hingga finishing yang sempurna.</p>
