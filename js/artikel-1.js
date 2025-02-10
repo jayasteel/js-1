c7acb720eb48e84f8d4b64afe974901085f3fba5
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:57:29 +0700

    Create artikel-1.js

---
+++ js/artikel-1.js
@@ -0,0 +1,2 @@
+<p>Untuk proyek konstruksi yang sukses, pemilihan material yang tepat sangat penting.<br />
+Jayasteel menyediakan berbagai produk yang memenuhi standar kualitas dan ketahanan tinggi, seperti besi beton dan wiremesh, yang siap memenuhi kebutuhan proyek Anda dengan harga kompetitif.</p>
