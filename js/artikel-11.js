e769a97d60acd95de8aefaacca73acde42c2b644
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 15:01:52 +0700

    Create artikel-11.js

---
+++ js/artikel-11.js
@@ -0,0 +1,2 @@
+<p>Memilih material yang tepat adalah langkah awal menuju bangunan yang kuat dan tahan lama.<br />
+Di Jayasteel, kami menawarkan produk berkualitas yang dirancang untuk mendukung setiap tahapan proyek konstruksi dan arsitektur Anda, dari struktur hingga finishing.</p>
