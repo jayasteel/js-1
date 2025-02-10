9978e20689fa04b610cf98e3d6c7ac344debfaf8
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:48:44 +0700

    Create artikel-17.js

---
+++ js/artikel-17.js
@@ -0,0 +1,2 @@
+<p>Untuk menciptakan bangunan yang kokoh dan estetis, Anda membutuhkan bahan bangunan yang berkualitas.<br />
+Jayasteel menawarkan berbagai material konstruksi yang dirancang untuk mendukung kebutuhan arsitektur modern, dengan kualitas yang telah teruji untuk setiap proyek Anda.</p>
