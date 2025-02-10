156d4c86b649b09b6dc62c3e66d9fdfe80165623
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 14:50:16 +0700

    Create besi-11.js

---
+++ js/besi-11.js
@@ -0,0 +1,2 @@
+<p>Jangan ragu untuk memilih <b>besi beton</b> berkualitas dari Jayasteel!<br />
+Kami menawarkan besi beton dengan berbagai ukuran, masing-masing dengan kekuatan dan daya tahan yang luar biasa, sehingga cocok untuk berbagai jenis pembangunan. Dapatkan penawaran menarik di sini!</p>
