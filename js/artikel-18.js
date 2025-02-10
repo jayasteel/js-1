eb08f2c04ced651c209e2102c13b128b185d1f5a
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 16:49:06 +0700

    Create artikel-18.js

---
+++ js/artikel-18.js
@@ -0,0 +1,2 @@
+<p>Konstruksi yang baik dimulai dengan bahan yang berkualitas.<br />
+Jayasteel hadir untuk menyediakan produk berkualitas yang memenuhi standar konstruksi dan arsitektur, dari besi beton hingga wiremesh, untuk mendukung kesuksesan proyek Anda.</p>
