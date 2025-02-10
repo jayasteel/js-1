baa26a199d046cc7309297b2d2779f8ad316826e
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 11:00:24 +0700

    Create besi-5.js

---
+++ js/besi-5.js
@@ -0,0 +1,2 @@
+<p>Apakah Anda sedang mencari tahu berapa berat <b>besi beton</b> ukuran 16 mm?<br />
+Besi beton dengan diameter 16 mm memiliki berat sekitar 18,94 kg. Ini bisa dihitung dengan menggunakan rumus berikut: 16 x 16 x 12 x 0,006165 = 18.93888. Setelah dibulatkan, beratnya adalah sekitar 18,94 kg. Informasi ini sangat berguna bagi Anda yang ingin menghitung kebutuhan besi untuk proyek konstruksi.</p>
