ee89a481e4f4293064c89c2c2713bff85dc99ec9
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 13:26:44 +0700

    Update wiremesh-0.js

--- js/wiremesh-0.js
+++ js/wiremesh-0.js
@@ -1,2 +1,2 @@
-<p>Apakah Anda mencari <b>wiremesh</b> berkualitas tinggi untuk proyek konstruksi Anda?<br />
+<p>Apakah Anda mencari <b>wiremesh</b> berkualitas untuk proyek konstruksi Anda?<br />
 Jayasteel menyediakan berbagai pilihan wiremesh yang kuat dan tahan lama, siap mendukung kebutuhan pembangunan Anda. Hubungi kami untuk mendapatkan wiremesh yang sesuai dengan kebutuhan proyek Anda.</p>
