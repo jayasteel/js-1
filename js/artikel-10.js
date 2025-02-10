3267fa3be58fd8ed459fc4f151362dff6abf2fff
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-10 15:01:28 +0700

    Create artikel-10.js

---
+++ js/artikel-10.js
@@ -0,0 +1,2 @@
+<p>Konstruksi yang baik dimulai dari perencanaan yang matang dan pemilihan material yang tepat.<br />
+Jayasteel siap membantu Anda dengan berbagai produk konstruksi yang memenuhi standar kualitas terbaik dan mendukung desain arsitektur terbaik untuk proyek Anda.</p>
