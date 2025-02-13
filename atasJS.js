82c59b06c2bfc2141d6d040577125559c4ad670e
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-13 09:01:46 +0700

    Update atasJS.js

--- atasJS.js
+++ atasJS.js
@@ -1,16 +1,3 @@
-function pf(j) {
-    const ul = document.querySelector('.n6');
-    j.feed.entry.slice(0, 2).forEach(({ title, link, summary, media$thumbnail }) => {
-        ul.innerHTML += `
-        <li>
-          <a href="${link.find(l => l.rel === 'alternate').href}" target="_blank" rel="nofollow" title="${title.$t}">
-            ${media$thumbnail ? `<img src="${media$thumbnail.url.replace(/s\d{2,4}-/, 's200-')}" alt="Thumbnail">` : ''}
-            <strong>${title.$t}</strong>
-          </a>
-        </li>
-      `;
-    });
-}
 document.write(`
 <style>
 .n6{padding-left:0;float:left;list-style:none;width:100%}
@@ -21,6 +8,19 @@ document.write(`
 .n6 strong {position:absolute;font-weight: bold;color:#fff;text-shadow: 2px 2px 4px #000;padding:22px;top:0;left:0}
 @media (min-width:1280px){.n6{width:250px;float:none}}
 </style>
-<ul class='n6'></ul>
-<div class='p3'><script src='https://www.jayasteel.com/feeds/posts/summary?max-results=2&alt=json&callback=pf'></script></div>
+<ul class="n6"></ul>
 `);
+  function pf(j) {
+    const ul = document.querySelector('.n6');
+    j.feed.entry.slice(0, 2).forEach(({ title, link, summary, media$thumbnail }) => {
+      ul.innerHTML += `
+        <li>
+          <a href="${link.find(l => l.rel === 'alternate').href}" target="_blank" rel="nofollow" title="${title.$t}">
+            ${media$thumbnail ? `<img src="${media$thumbnail.url.replace(/s\d{2,4}-/, 's200-')}" alt="Thumbnail">` : ''}
+            <strong>${title.$t}</strong>
+          </a>
+        </li>
+      `;
+    });
+  }
+document.write('<div class="p3"><script src="https://www.jayasteel.com/feeds/posts/summary?max-results=2&amp;alt=json&amp;callback=pf"></' + 'script></div>');
