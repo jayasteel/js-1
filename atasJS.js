21f2bdbe5c674a25f2b44af0f039bee2aeb29fe2
jayasteel <148539642+jayasteel@users.noreply.github.com>
2025-02-13 07:26:05 +0700

    Update atasJS.js

--- atasJS.js
+++ atasJS.js
@@ -13,4 +13,4 @@ document.getElementById("u5").innerHTML = "<style>.n6{padding-left:0;float:left;
     });
   }
 document.getElementById("u5").insertAdjacentHTML("afterend",
-"<div class='p3'><script src="https://www.jayasteel.com/feeds/posts/summary?max-results=2&amp;alt=json&amp;callback=pf"></" + "script></div>";
+"<div class='p3'><script src='https://www.jayasteel.com/feeds/posts/summary?max-results=2&amp;alt=json&amp;callback=pf'></" + "script></div>";
